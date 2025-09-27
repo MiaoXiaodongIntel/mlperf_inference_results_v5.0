3f355f8d52ee6deb2baeb467cab3ec0ea8ad3ea1
Miao, Xiaodong <xiaodong.miao@intel.com>
2025-09-27 15:20:11 +0800

    save wget log to local file for debug

--- closed/NVIDIA/code/common/file_downloads.sh
+++ closed/NVIDIA/code/common/file_downloads.sh
@@ -27,7 +27,8 @@ function download_file {
         echo "Creating directory ${_SUB_DIR}"
         mkdir -p ${_SUB_DIR}
     fi
-    echo "Downloading $2 $1..." \
-        && wget $3 -O ${_SUB_DIR}/$4 \
-        && echo "Saved $2 $1 to ${_SUB_DIR}/$4!"
+    echo "Downloading $2 $1..."
+    wget $3 -O ${_SUB_DIR}/$4 2>&1 | tee ${_SUB_DIR}/wget_${4}.log
+    # curl -o ${_SUB_DIR}/$4 -L $3
+    echo "Saved $2 $1 to ${_SUB_DIR}/$4!"
 }
